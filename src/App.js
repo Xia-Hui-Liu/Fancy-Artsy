fb8b62543fae5de78cc02c5814622e3c94e6a822
Xiahui Liu <98664540+newbieherecs@users.noreply.github.com>
2023-02-08 09:43:10 +0100

    move folder to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from './components/NavBar';
+import Main from './components/Main';
+
+const App = () => {
+  return (
+      <div>
+        <NavBar />
+          <hr className='hr'></hr>
+        <Main />
+      </div>
+  );
+  }
+
+export default App;
+      
